a7774b27470d303cbccaa42219e30d81bdca7b82
s21016 <s21016@std.it-college.ac.jp>
2022-08-03 10:08:03 +0900

    Initial commit

---
+++ app/src/main/java/jc/ac/it_college/std/s21016/pbl_app/MainActivity.kt
@@ -0,0 +1,11 @@
+package jc.ac.it_college.std.s21016.pbl_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
