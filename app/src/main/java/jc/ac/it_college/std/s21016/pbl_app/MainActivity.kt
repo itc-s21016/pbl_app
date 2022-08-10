316904b4c589f9df1d2c379c0b50a37382efdfe8
s21016 <s21016@std.it-college.ac.jp>
2022-08-10 09:45:01 +0900

    strings.xmlにそれぞれの星座を追加

--- app/src/main/java/jc/ac/it_college/std/s21016/pbl_app/MainActivity.kt
+++ app/src/main/java/jc/ac/it_college/std/s21016/pbl_app/MainActivity.kt
@@ -2,10 +2,22 @@ package jc.ac.it_college.std.s21016.pbl_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import jc.ac.it_college.std.s21016.pbl_app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
+
 }
+
+    //戻るボタン
+    //後ほどサブ画面に書く
+    /*binding.backButton.setOnClickListener {
+        finish()
+    }*/
